1c6be0d4d9397eda6edce6ba7cce631c76e8ff58
klords <klords@asu.edu>
2014-10-08 04:52:43 -0700

    Took out debug messages from DebrisField class

--- Asteroids/src/tilemap/DebrisField.java
+++ Asteroids/src/tilemap/DebrisField.java
@@ -16,10 +16,8 @@ public class DebrisField implements Drawable{
 		debrisField = new ArrayList<Images>();
 		debrisField.add(new Images("/resources/asteroids/debrisField0_1.png"));
 		fieldHeight = debrisField.get(0).getHeight();
-		System.out.println(fieldHeight);
 		debrisField.get(0).setAutoScroll(0, autoScrollAmt);
 		debrisField.get(0).setPosition(0, GamePanel.HEIGHT - fieldHeight);
-		System.out.println(GamePanel.HEIGHT / fieldHeight);
 		for (int i = 1; i < (GamePanel.HEIGHT / fieldHeight) + 1; i++) {
 			debrisField.add(new Images("/resources/asteroids/debrisField0_1.png"));
 			debrisField.get(i).setPosition(0, GamePanel.HEIGHT - (fieldHeight * (i + 1)));
